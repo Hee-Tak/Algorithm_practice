897fd6da36871383795f22e303369533060774ce
hitak77 <hitak77@naver.com>
2023-04-23 09:28:35 +0900

    크루스칼, 프림, 보르우카

--- MinimumSpanningTree/src/main/kotlin/basic.kt
+++ MinimumSpanningTree/src/main/kotlin/basic.kt
@@ -33,8 +33,67 @@ fun main(){
     for(edge in mst){
         println("${edge.u} - ${edge.v} : ${edge.weight}")
     }
+    println("=============================")
     //==========================================
+    //Boruvka Algorithm
+    val graph2 = listOf(
+        Edge2(0, 1, 4),
+        Edge2(0, 7, 8),
+        Edge2(1, 2, 8),
+        Edge2(1, 7, 11),
+        Edge2(2, 3, 7),
+        Edge2(2, 8, 2),
+        Edge2(2, 5, 4),
+        Edge2(3, 4, 9),
+        Edge2(3, 5, 14),
+        Edge2(4, 5, 10),
+        Edge2(5, 6, 2),
+        Edge2(6, 7, 1),
+        Edge2(6, 8, 6),
+        Edge2(7, 8, 7)
+    )
+    /* 생김새
+        4       8
+      0 --- 1 --- 7
+      |  \  /|
+     8|   11 |4
+      |  /  \|
+      2 --- 3 --- 4
+           |  9
+           |   \
+           14   10
+            |  /
+            5 --- 6
+            |   2
+            |   |
+            6 - 8
 
+     */
+    val mst2 = boruvka(graph2, 9)
+    // [Edge(src=6, dest=7, weight=1), Edge(src=2, dest=8, weight=2),
+    // Edge(src=5, dest=6, weight=2), Edge(src=0, dest=1, weight=4),
+    // Edge(src=2, dest=5, weight=4), Edge(src=2, dest=3, weight=7),
+    // Edge(src=0, dest=7, weight=8), Edge(src=3, dest=4, weight=9)]
+    /*
+         4       8
+      0 --- 1     |
+                7 |
+                    |
+      |              |
+     8|          11 |4
+      |              |
+      2 --- 3       |
+      |       \     |
+     4|        14\   |10
+      |          \  |
+      5 --- 6 --- 7 |
+            2     1 |
+             \     \|
+              6 --- 8
+     */
+    for(edge in mst2){
+        println("${edge.src} - ${edge.dest} : ${edge.weight}")
+    }
 }
 
 
