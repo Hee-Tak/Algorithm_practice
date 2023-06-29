2707175cc048ee4c757a89bd7146fb93908bf427
hitak77 <hitak77@naver.com>
2023-06-30 05:57:42 +0900

    level 0) 더 크게 합치기

---
+++ moreBiggerSum/src/main/kotlin/m.kt
@@ -0,0 +1,16 @@
+class Solution {
+    fun solution(a: Int, b: Int): Int {
+        var answer: Int = 0
+        var x = (a.toString() + b.toString()).toInt()
+        var y = (b.toString() + a.toString()).toInt()
+        return if( x >= y){
+            x
+        } else{
+            y
+        }
+    }
+}
+/**
+ * level 0) 더 크게 합치기
+ * https://school.programmers.co.kr/learn/courses/30/lessons/181939
+ */
