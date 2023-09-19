7ceeb5dc0c026fb31a03365800317ed6b0e9cc54
hitak77 <hitak77@naver.com>
2023-09-20 03:47:17 +0900

    아이스크림

---
+++ meltIcecreamTop/src/main/kotlin/Solution.kt
@@ -0,0 +1,23 @@
+import java.util.ArrayDeque
+class Solution {
+    public fun solution(preferences: String): String {
+        val iceCream = arrayOf("C", "M", "S", "V", "Y")
+        val stack = ArrayDeque<String>()
+    }
+
+    private fun trigger(people: String, target: String): Boolean {
+        var taste = people
+        var icecream = target
+
+        //임시값
+        taste = "CS"
+        icecream = taste
+    }
+}
+
+//입력값                               출력값
+//preferences
+//["CS", "SV"]                    ->  "CCVS"
+//["SV", "VS", "SV", "VS"]        ->  "SV"
+//["CM", "MS", "SC"]              ->  "CCMMS"
+// 위층때매 녹아서 맛 합쳐진걸(침식당한걸) 어떻게 생각해봐야할듯
